13efc83c2a2feecf0b35f07cdb28aaa6eaaaf5ba
Gabbster94 <b194gab@gmail.com>
2018-08-25 17:21:46 +0300

    Added ReadFromFile feature

--- src/baseFunctions/GetCredentialsFromFile.java
+++ src/baseFunctions/GetCredentialsFromFile.java
@@ -1,5 +1,42 @@
 package baseFunctions;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class GetCredentialsFromFile {
 
+	public static Properties readfile() throws IOException {
+		File file = new File("C:\\_DiskD\\Programare\\Git\\PrivateConfig\\Credentials.properties");
+
+		FileInputStream fileInput = null;
+		fileInput = new FileInputStream(file);
+
+		Properties prop = new Properties();
+
+		// load properties file
+		prop.load(fileInput);
+
+		return prop;
+	}
+
+	public static String getUsernameFromFile(String App) throws IOException {
+		Properties prop = readfile();
+		if (App.equals("epiesa")) {
+			String Username = prop.getProperty("epiesaEmail");
+			return Username;
+		}
+		return null;
+
+	}
+
+	public static String getPasswordFromFile(String App) throws IOException {
+		Properties prop = readfile();
+		if (App.equals("epiesa")) {
+			String Password = prop.getProperty("epiesaPassword");
+			return Password;
+		}
+		return null;
+	}
 }

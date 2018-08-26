8792caa1af11a4bacebdeeb6ed51c26f5ee9e224
Gabbster94 <b194gab@gmail.com>
2018-08-26 16:47:16 +0300

    Implemented EpiesaFunctions with several methods. Needs to be implemented ShippingDetails method in EpiesaFunctions.

--- src/baseFunctions/TestClasses.java
+++ src/baseFunctions/TestClasses.java
@@ -6,7 +6,7 @@ public class TestClasses {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		System.out.println(GetCredentialsFromFile.getPasswordFromFile("epiesa"));
+		System.out.println(GetDataFromFile.getPasswordFromFile("epiesa"));
 	}
 
 }
